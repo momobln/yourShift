eb5d2530734d63dcf0fbe8bc8c2f8917955c7cb8
Laura Sinclair <l.sinclair@tuta.io>
2025-10-08 16:00:01 +0200

    Remove ! at the end of .env variables

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -7,19 +7,19 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const handler = NextAuth({
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ClIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        }),
-        GitHubProvider({
-            clientId: process.env.GITHUB_CLIENT_ID!,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-        }),
-    ],
-    session: { strategy: "jwt" },
-    secret: process.env.NEXTAUTH_SECRET,
+	adapter: PrismaAdapter(prisma),
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+		}),
+		GitHubProvider({
+			clientId: process.env.GITHUB_CLIENT_ID,
+			clientSecret: process.env.GITHUB_CLIENT_SECRET,
+		}),
+	],
+	session: { strategy: "jwt" },
+	secret: process.env.NEXTAUTH_SECRET,
 });
 
 export { handler as GET, handler as POST};
